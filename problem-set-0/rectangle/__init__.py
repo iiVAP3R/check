11c1565f559e8f8f7ee31de52c27b4ad1488e811
crypd007 <146155598+crypd007@users.noreply.github.com>
2024-08-10 09:03:39 +0000

    updated __init__.py

--- problem-set-0/rectangle/__init__.py
+++ problem-set-0/rectangle/__init__.py
@@ -1,4 +1,6 @@
 import check50
+import re
+
 
 @check50.check()
 def fileExists():
@@ -10,16 +12,27 @@ def fileExists():
 @check50.check(fileExists)
 def test0():
     program_output = check50.run("python3 rectangle.py").stdin(5).stdin(5).stdout()
+    program_output = re.search(r"(\d+)", program_output)
+    if not program_output:
+        raise check50.Failure("Output does not contain a numeric value")
+    
+    program_output = int(program_output.group(1))
     expected_output = 25
+    
     if program_output != expected_output:
         help = "W = 5, H = 5, expected 25"
-        check50.Mismatch(expected_output, program_output, help=help)
+        raise check50.Mismatch(expected_output, program_output, help=help)
 
 
 @check50.check(fileExists)
 def test1():
     program_output = check50.run("python rectangle.py").stdin(6).stdin(9).stdout()
+    program_output = re.search(r"(\d+)", program_output)
+    if not program_output:
+        raise check50.Failure("Output does not contain a numeric value")
+    
+    program_output = int(program_output.group(1))
     expected_output = 54
     if program_output != expected_output:
         help = "W = 6, H = 9, expected 54"
-        check50.Mismatch(expected_output, program_output, help=help)
+        raise check50.Mismatch(expected_output, program_output, help=help)
