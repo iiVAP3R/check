9d65fe62cefb3a5e4f3d16e538c8b6d1b1dc8873
crypd007 <146155598+crypd007@users.noreply.github.com>
2024-08-11 01:36:44 +0000

    fixed bug in rectangle check REAL THIS TIME

--- problem-set-0/syntax-errors/__init__.py
+++ problem-set-0/syntax-errors/__init__.py
@@ -10,7 +10,7 @@ def fileExists():
 def test0():
     """ Tests if the program runs as expected (H = 6, R = 9, expected = 508.94)"""
     program_output = check50.run("python syntax-errors.py").stdin("6").stdin("9").stdout()
-    program_output = re.search(r"(\d+)", program_output)
+    program_output = re.search(r"(\d+\.\d+)", program_output)
     if not program_output:
         raise check50.Failure("Output does not contain a numeric value")
     
@@ -25,7 +25,7 @@ def test0():
 def test1():
     """ Tests if the program runs as expected (H = 2, R = 2, expected 8.38)"""
     program_output = check50.run("python syntax-errors.py").stdin("2").stdin("2").stdout()
-    program_output = re.search(r"(\d+)", program_output)
+    program_output = re.search(r"(\d+\.\d+)", program_output)
     if not program_output:
         raise check50.Failure("Output does not contain a numeric value")
     
@@ -40,7 +40,7 @@ def test1():
 def test2():
     """ Tests if the program runs as expected (H = 4, R = 20, expected 335.1)"""
     program_output = check50.run("python syntax-errors.py").stdin("4").stdin("20").stdout()
-    program_output = re.search(r"(\d+)", program_output)
+    program_output = re.search(r"(\d+\.\d+)", program_output)
     if not program_output:
         raise check50.Failure("Output does not contain a numeric value")
     
@@ -49,3 +49,4 @@ def test2():
 
     if program_output != expected_output:
         raise check50.Mismatch(str(expected_output), str(program_output))
+    
