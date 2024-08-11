a87dd82a33843f119ea5cdf09d105356a132b35c
crypd007 <146155598+crypd007@users.noreply.github.com>
2024-08-11 12:13:31 +0000

    attempting to fix bug for triangle check again

--- problem-set-1/triangle/__init__.py
+++ problem-set-1/triangle/__init__.py
@@ -60,7 +60,36 @@ def check_input_12():
 
 
 def check_output(expected_output, program_output):
-    if expected_output == program_output:
-        return
+    # split expected and actual outputs into lines (into a list)
+    expected_lines = expected_output.splitlines()
+    output_lines = program_output.splitlines()
 
-    raise check50.Mismatch(expected_output, program_output, help="Does your triangle match the example triangle form? Does does the height match the input?")
+
+    # remove any empty lines from the actual output
+    cleaned_output_lines = []
+    for line in output_lines:
+        if line != "":
+            cleaned_output_lines.append(line)
+
+
+    # check if num of lines match for expected and input
+    if len(cleaned_output_lines) != len(expected_lines):
+        raise check50.Mismatch(expected_lines, cleaned_output_lines, help="The number of lines in the output does not match the expected output.")
+
+    # compare each line of expected to input
+    for expected_line, output_line in zip(expected_lines, cleaned_output_lines):
+        if expected_line != output_line:
+            # check for trailing whitespace issues
+            if expected_line.rstrip() == output_line.rstrip():
+                help = "Did you add too much trailing whitespace to the end of your triangle?"
+            # check for additional characters at the beginning of each line
+            elif expected_line == output_line[1:]:
+                help = "Are you printing an additional character at the beginning of each line?"
+            else:
+                help = "The output does not match the expected format."
+
+            raise check50.Mismatch(expected_lines, cleaned_output_lines, help=help)
+
+
+    # if match, return correct
+    return
